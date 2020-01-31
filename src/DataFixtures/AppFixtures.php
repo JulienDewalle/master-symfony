49953607322a9501235670af04735f730ae07868
Dewalle Julien <julien.dewalle@hotmail.fr>
2020-01-31 14:38:44 +0100

    product relation with user and fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,30 +3,49 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\MakerBundle\Maker\MakeEntity;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Doctrine\Migrations\Version\Factory;
 
 class AppFixtures extends Fixture
 {
     /**
      * @var SluggerInterface
      */
-    public function __contruct(SluggerInterface $slugger){
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger){
 
         $this->slugger = $slugger;
 
     }    
 
+
     public function load(ObjectManager $manager)
     {
+        $faker= \Faker\Factory::create('fr_FR');
+
+        //on crée les utilisateurs
+        $users = []; // Le tableau va nous aider à stocker les instances des users
+        for ($i=0; $i <= 10; $i++) {
+            $user = new User();
+            $user->setUsername($faker->email);
+            $manager->persist($user); 
+            $users[] = $user;       
+        }
+
+
+        //on crée les produits
         for ($i=0; $i <= 100; $i++) {
         $product = new Product();
         $product->setName('iPhone '.$i);
-        $product->setSlug($this->Slugger->slug($product->getName())->lower());
+        $product->setSlug($this->slugger->slug($product->getName())->lower());
         $product->setDescription('Un iPhone de '.rand(2000, 2020));
         $product->setPrice(rand(10, 100)*100);
+        $product->setUser($users[rand(0, 9)]);
         $manager->persist($product);
     }
 
